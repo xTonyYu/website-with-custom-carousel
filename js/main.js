e205ebf083dc0c1e68756d05a111592af0393611
Tony Yu <xtonyyu@yahoo.com>
2020-05-05 17:04:43 -0700

    carousel but need to limit slide left to window width and not continue till all the cards are gone

--- js/main.js
+++ js/main.js
@@ -2,7 +2,14 @@ const searchContainer = document.querySelector('.search-container');
 const whiteOut = document.querySelectorAll('.white-out');
 const search = document.querySelector('.search');
 const listContainer = document.querySelector('.list-container');
-const teammember = document.querySelectorAll('.teammember');
+const leftButton = document.querySelector('.left-btn');
+const rightButton = document.querySelector('.right-btn');
+const teammemberList = document.querySelector('.teammember-list');
+const teammembers = document.querySelectorAll('.teammember');
+const teammembersArr = new Array(...teammembers);
+console.log("teammembersArr", teammembersArr);
+teammembersArr.map(member => member._showing = true);
+
 const uxList = [
     {title: 'UX Designer', url: '../PlumPear/designer.html'},
     {title: 'UX Researcher', url: '#researcher'},
@@ -52,19 +59,68 @@ function toggleBkgrnd(e) {
         })
     }
 }
+
+function totalWidth(leadingCard) {
+    if (leadingCard === -1) {
+        return -1;
+    }
+    const curStyle = leadingCard.currentStyle || window.getComputedStyle(leadingCard),
+    width = leadingCard.offsetWidth, // or use curStyle.width
+    margin = parseFloat(curStyle.marginLeft) + parseFloat(curStyle.marginRight),
+    padding = parseFloat(curStyle.paddingLeft) + parseFloat(curStyle.paddingRight),
+    border = parseFloat(curStyle.borderLeftWidth) + parseFloat(curStyle.borderRightWidth);
+    return (width + padding + border + margin);
+}
+
+function slideLeftOrRight(e, slideDirection) {
+    // 
+    console.dir(e.target.className);
+    const leadingCardIndex = teammembersArr.findIndex(member => member._showing === true);
+    let leadingCard, newMarginLeft;
+    const curMarginLeft = parseFloat(teammemberList.style.marginLeft) || 0;
+    
+    
+    console.log("teammembersArr", teammembersArr);
+    console.log("%ccurMarginLeft:", 'color: blue', curMarginLeft);
+    console.log("marginLeft BEFORE:",teammemberList.style.marginLeft);
+    
+    if (slideDirection === 'left') {
+        leadingCard = teammembersArr[leadingCardIndex];
+        leadingCard._showing = false;
+        newMarginLeft = curMarginLeft - totalWidth(leadingCard) -1 + 'px';
+    } else {
+        leadingCard = teammembersArr[leadingCardIndex-1] || -1;
+        leadingCard._showing = true;
+        newMarginLeft = curMarginLeft + totalWidth(leadingCard) +1 + 'px';
+    }
+    
+    console.log(leadingCard.innerText, "- total width", totalWidth(leadingCard));
+    console.log("newMarginLeft", newMarginLeft);
+    teammemberList.style.setProperty('margin-left', newMarginLeft);
+    console.log("%cmargLeft AFTER:", 'color: red', teammemberList.style.marginLeft);
+
+}
+
 searchContainer.addEventListener('mouseover', toggleBkgrnd);
 searchContainer.addEventListener('mouseleave', toggleBkgrnd);
 search.addEventListener('keyup', filteredList);
+leftButton.addEventListener('click', e => slideLeftOrRight(e, 'right'));
+rightButton.addEventListener('click', e => slideLeftOrRight(e, 'left'));
 
-teammember.forEach(member => member.addEventListener('mouseover', inspect));
+teammembers.forEach(member => member.addEventListener('mouseover', inspect));
 
+// teammembers[1].setAttribute('hidden','');
+console.dir(teammemberList);
 
-// teammember[1].setAttribute('hidden','');
 function inspect(e) {
     // console.log(e);
     // console.dir(e);
     console.log(this.innerText);
     console.dir(this);
-    console.log(teammember[5].innerText);
-    console.dir(teammember[5]);
+    // console.log(teammembers[5].innerText);
+    // console.dir(teammembers[5]);
+    // console.log(teammembers[2].innerText);
+    // console.dir(teammembers[2]);
+    // leadingEleHideOrShow(totalWidth);
 }
+
